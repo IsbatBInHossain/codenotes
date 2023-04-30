98248c2a640309f5346a911dc7784cb35a96c34e
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-04-30 16:59:03 +0600

    Add basic transpiling functionality with esbuild-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+
+import * as esbuild from 'esbuild-wasm';
+
+await esbuild.initialize({
+  worker: true,
+  wasmURL: 'esbuild.wasm',
+});
+
 function App() {
-  return <>CodeNotes</>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    esbuild
+      .transform(input, {
+        loader: 'tsx',
+        target: 'es2015',
+      })
+      .then(result => {
+        setCode(result.code);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
