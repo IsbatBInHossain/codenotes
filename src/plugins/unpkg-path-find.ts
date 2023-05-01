7eb45e6a53968b029cc13121d83b04932d476647
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-05-01 22:17:33 +0600

    Add caching layer

--- src/plugins/unpkg-path-find.ts
+++ src/plugins/unpkg-path-find.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'fileCache',
+});
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
         console.log('onResolve:', args);
 
         if (args.path === 'index.js') {
@@ -24,7 +29,7 @@ export const unpkgPathPlugin = () => {
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
         console.log('onLoad:', args);
 
         if (args.path === 'index.js') {
@@ -37,12 +42,22 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
