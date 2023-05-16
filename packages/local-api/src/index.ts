f76a1ecce0ab2e364c4bccfed2e668903fdbd730
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-05-16 20:58:19 +0600

    Add basic cli package logics

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server has started');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('Server started on port', port);
+  console.log('Fetching file from', filename);
+  console.log('File location', dir);
 };
